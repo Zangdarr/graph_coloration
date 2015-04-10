d2b4cce5d01f75c4e920cca07fc32422a32a6cbe
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-10 10:45:51 +0200

    Ajout de la classe Coloration.java.
    Cette classe représente la coloration d'un graphe. Elle contient le graphe colorer, la liste des couleurs ainsi que l'association des sommets à leur couleur respective.

---
+++ src/tools/Coloration.java
@@ -0,0 +1,85 @@
+package tools;
+
+import graphe.Graphe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Coloration {
+
+    private Graphe g;
+    private HashMap<Integer, Color> color; // map vertexID/color
+    private HashMap<Integer,Color> colorList; //color list
+    
+    
+    /**
+     * 
+     * @param g
+     * @param color
+     * @param colorList
+     */
+    public Coloration(Graphe g, HashMap<Integer, Color> color, HashMap<Integer,Color> colorList) {
+        super();
+        this.g = g;
+        this.color = color;
+        this.colorList = colorList;
+    }
+    
+    public HashMap<Integer,Integer> getTabColorCounter(){
+        HashMap<Integer,Integer> colorCounterList = new HashMap<Integer,Integer>();
+        for(int colorID : colorList.keySet()){
+            colorCounterList.put(colorID,0);
+        }
+        
+        for(int i : color.keySet()){
+            int colorID = color.get(i).getId();
+            colorCounterList.put(colorID, colorCounterList.get(colorID) + 1);
+        }
+        
+        return colorCounterList;
+        
+    }
+
+
+    
+    public String toStringMinimal(){
+        StringBuffer result = new StringBuffer("");
+        result.append("Nombre de couleur : ");
+        result.append(colorList.size()-1);
+        result.append('\n');
+        
+        HashMap<Integer,Integer> list = getTabColorCounter();
+        
+        for (int i : list.keySet()) {
+            result.append(" - ");
+            result.append(colorList.get(i).getName());
+            result.append(" : ");
+            result.append(list.get(i));
+            result.append("\n");
+        }
+        
+        return result.toString();
+    }
+    
+    
+    public String toString(){
+        StringBuffer result = new StringBuffer("");
+        result.append("Nombre de couleur : ");
+        result.append(colorList.size()-1);
+        result.append('\n');
+        
+        
+        
+        for (Iterator<Integer> iterator = color.keySet().iterator();iterator.hasNext();){
+            int id = iterator.next();
+            result.append("- Vertex " + id + " ");
+            result.append(color.get(id).getName());
+            result.append('\n');
+
+        }
+        
+        
+        return result.toString();
+    }
+}
