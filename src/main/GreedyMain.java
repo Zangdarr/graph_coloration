1973c821b570225b808ad62becd8c4f9db3246f2
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-11 15:08:21 +0200

    Ajout du main appliquant une heuristique gloutonne pour la coloration de graphe.

---
+++ src/main/GreedyMain.java
@@ -0,0 +1,38 @@
+package main;
+
+import exceptions.EdgeAlreadyExistException;
+import exceptions.VertexAlreadyExistException;
+import exceptions.VertexNotFoundException;
+import graphe.Graphe;
+
+import java.io.IOException;
+
+import tools.Coloration;
+import tools.ColorationTools;
+import tools.GraphTools;
+
+public class GreedyMain {
+
+    public static void main(String[] args) {
+        callGreedy("1000x70.gph");
+    }
+
+
+    public static void callGreedy(String graphFileName){
+        System.out.println("GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  ");
+        Graphe g = new Graphe();
+        try {
+            g = GraphTools.fileToGraph(graphFileName);
+        } catch (NumberFormatException | IOException
+                | VertexAlreadyExistException | VertexNotFoundException
+                | EdgeAlreadyExistException e2) {
+            System.out.println(e2.getMessage());
+        }
+        Coloration result;
+        result = ColorationTools.greedyColoration(g);
+        System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nResult of Greedy coloration, " + result.toStringMinimal());
+
+        System.out.println("GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  GREEDY  ");
+
+    }
+}
