e1730305abc8ec97baa92fe4e2948bcf826d3516
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-18 16:30:29 +0200

    Mise en évidence des paramètre de génération de graphe.

--- src/main/FileGen_Main.java
+++ src/main/FileGen_Main.java
@@ -9,14 +9,23 @@ import graphe.RandomGraphGenerator;
 public class FileGen_Main {
 
     public static void main(String[] args) {
+
+        //CONFIGURATION
+        int nbSommet = 1000;
+        float proba = (float) 1.0;
+        String filename = nbSommet +"x"+ (int)(proba*100) +".gph";
+
+
+
         RandomGraphGenerator gen = new RandomGraphGenerator();
         System.out.println("START");
-        Graphe g = gen.generateErdosRenyiGraph(10,(float) 1.0);
+        Graphe g = gen.generateErdosRenyiGraph(nbSommet,proba);
         try {
-            GraphTools.graphToFile("10x100.gph", g);
+            GraphTools.graphToFile(filename, g);
         } catch (IOException e) {
             System.err.println("Erreur lors de la création d'un fichier graphe. " + e.getMessage());
         }
+
     }
 
 }
